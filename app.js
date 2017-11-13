0bd54f8d0f86faf9d7317e93d52df9208a34ca3f
rahul <rahulsingh2240@gmail.com>
2017-11-13 11:59:46 +0530

    setup pug and views

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const path = require('path');
 
+// Init App
 const app = express();
 
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+// Home route
 app.get('/', function(req, res){
-	res.send("hello world!");
+	res.render("index");
 });
 
+// Start Server
 app.listen(3000, function(){
 	console.log("server started on port 3000");
 });
